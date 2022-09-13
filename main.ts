d41ecfc4a2760da7c9bd84a3836afb7e6aa39d1b
SlamaJamaSam <112981132+SlamaJamaSam@users.noreply.github.com>
2022-09-13 12:23:21 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,55 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Display = randint(0, 1)
+    if (Display == 0) {
+        basic.showString("Truth")
+    }
+    if (Display == 1) {
+        basic.showString("Dare")
+    }
+    basic.pause(2000)
+    basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    Arrow = randint(1, 4)
+    if (Arrow == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (Arrow == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (Arrow == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (Arrow == 4) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    basic.pause(2000)
+    basic.clearScreen()
+})
+let Arrow = 0
+let Display = 0
+basic.showString("Hello! Wellcome to Truth or Dare")
